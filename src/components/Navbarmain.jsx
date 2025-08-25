3ff15310098597fc2a98514c4ea6aabeb4e65b8b
Alan Biju <alan.biju@mwttech.com>
2025-08-25 18:07:35 +0530

    Created Navbar

---
+++ src/components/Navbarmain.jsx
@@ -0,0 +1,36 @@
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { BsBagFill, BsPersonCircle } from "react-icons/bs";
+
+const Navbarmain = () => {
+
+    
+
+    return(
+        <>
+            <div style={{ background: "rgb(247, 244, 241)" }} className="">
+                <Navbar style={{ background: "rgb(18, 29, 46)", height:'65px'}}>
+                    <Container>
+                        <Navbar.Brand className="m-0">
+                            <h2 className="m-0 text-light">LOGO</h2>
+                        </Navbar.Brand>
+                        <Nav className="ps-5 d-flex align-item-center justify-content">
+                            <Nav.Link className="text-light fw-semibold ps-4" >Home</Nav.Link>
+                            <Nav.Link className="text-light fw-semibold ps-4" >Men</Nav.Link>
+                            <Nav.Link className="text-light fw-semibold ps-4" >Women</Nav.Link>
+                            <Nav.Link className="text-light fw-semibold ps-4" >Children</Nav.Link>
+                            <Nav.Link className="text-light fw-semibold ps-4 pb-2 " >
+                                <BsBagFill className="me-2 text-light" style={{ fontSize: '1.5rem'}}/>
+                            </Nav.Link>
+                            <Button className="ms-2" style={{height: '30px', margin: 'auto', background: 'white', border: 'none', color: 'rgb(18, 29, 46)', paddingTop: '3px', fontWeight: '900', borderRadius: '10px'}}>Login</Button>
+                            <Nav.Link className="text-light fw-semibold ps-4 pb-2 " >
+                                <BsPersonCircle className="me-2 text-light" style={{ fontSize: '1.5rem'}}/>
+                            </Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </div>
+        </>
+    );
+}
+
+export default Navbarmain;
